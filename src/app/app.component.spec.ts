58e90fb0de556626c4a067ba91ad6421b61caf63
argun8394 <argundoganali@gmail.com>
2022-06-21 00:23:52 +0300

    Angular e-commerce_template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'aaaaaaaa'`, () => {
+  it(`should have as title 'template_ecommerce'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('aaaaaaaa');
+    expect(app.title).toEqual('template_ecommerce');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('aaaaaaaa app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('template_ecommerce app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FeatureSliderComponent } from './components/feature-slider/feature-slider.component';
+import { ProductGridComponent } from './components/product-grid/product-grid.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { BestSellerComponent } from './components/best-seller/best-seller.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    FeatureSliderComponent,
+    ProductGridComponent,
+    BannerComponent,
+    BestSellerComponent,
+    FooterComponent,  ],
   imports: [
     BrowserModule,
     AppRoutingModule
